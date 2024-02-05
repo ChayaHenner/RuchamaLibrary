53a985c4b89e443e533b9f31c137859f3779bfe0
ChayaHenner <chayahenner@gmail.com>
2024-02-05 12:45:13 +0200

    s

--- server/src/service/reader.service.ts
+++ server/src/service/reader.service.ts
@@ -1,7 +1,7 @@
 import { getReadersDB, postReaderDB, softDeleteDB } from '../repository/reader.repository';
 
-export const getReaders = async (searchTerm: string) => {return await getReadersDB(searchTerm);};
+export const getReaders = async (searchTerm: string) => { return await getReadersDB(searchTerm); };
 
-export const postReader = async (reader: any) => {return  await postReaderDB(reader);};
+export const postReader = async (reader: any) => { return await postReaderDB(reader); };
 
-export const softDelete = async (id: number) => {return await softDeleteDB(id);};
+export const softDelete = async (id: number) => { return await softDeleteDB(id); };
