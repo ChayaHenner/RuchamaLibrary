71c06b5a8cffc2b47639c1c06eb8cdabca00a8ae
ChayaHenner <132666389+ChayaHenner@users.noreply.github.com>
2025-11-05 14:24:02 +0200

    Update .gitignore and refactor app.js for improved structure; adjust package.json scripts for build process

--- server/dist/app.js
+++ server/dist/app.js
@@ -1,21 +1,44 @@
-'use strict'
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod }
-  }
-Object.defineProperty(exports, '__esModule', { value: true })
-const express_1 = __importDefault(require('express'))
-const index_1 = __importDefault(require('./routes/index'))
-require('reflect-metadata')
-const db_1 = __importDefault(require('./repository/db'))
-const app = (0, express_1.default)()
-const port = process.env.PORT || 3000
-// app.get('/', function (req, res) {
-//     res.send('Hello World!!')
-// })
-;(0, db_1.default)()
-app.use('/', index_1.default)
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`)
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.libraryData = void 0;
+const express_1 = __importDefault(require("express"));
+const route_const_1 = __importDefault(require("./routes/route.const"));
+require("reflect-metadata");
+require("dotenv/config");
+const Reader_1 = require("./entities/Reader");
+const typeorm_1 = require("typeorm");
+const Publisher_1 = require("./entities/Publisher");
+const Book_1 = require("./entities/Book");
+const BookInstance_1 = require("./entities/BookInstance");
+const Borrowing_1 = require("./entities/Borrowing");
+const cors_1 = __importDefault(require("cors"));
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+const port = process.env.PORT || 5000;
+app.use(express_1.default.json());
+exports.libraryData = new typeorm_1.DataSource({
+    type: 'postgres',
+    host: 'localhost',
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    logging: true,
+    entities: [Reader_1.Reader, Publisher_1.Publisher, BookInstance_1.BookInstance, Book_1.Book, Borrowing_1.Borrowing],
+    synchronize: true,
+});
+exports.libraryData
+    .initialize()
+    .then(() => {
+    console.log('✅ Database connected');
+    app.use('/', route_const_1.default);
+    app.listen(port, () => {
+        console.log(`Server is listening on PORT ${port}`);
+    });
 })
+    .catch((err) => {
+    console.error('Database connection error:', err);
+});
