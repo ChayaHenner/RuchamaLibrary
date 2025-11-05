79f2a61aeb79e79f669bcd5b4190ca436c17b6d6
ChayaHenner <132666389+ChayaHenner@users.noreply.github.com>
2025-11-05 15:39:50 +0200

    Enhance CORS configuration to allow specific client origins

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,8 +10,15 @@ import { BookInstance } from './entities/BookInstance'
 import { Borrowing } from './entities/Borrowing'
 import cors from 'cors'
 
+const clientUrl = process.env.CLIENT_URL || 'http://localhost:5173'
+
 const app = express()
-app.use(cors())
+app.use(
+  cors({
+    origin: [clientUrl, 'http://localhost:5173'],
+    credentials: true,
+  }),
+)
 const port = process.env.PORT || 5000
 
 app.use(express.json())
